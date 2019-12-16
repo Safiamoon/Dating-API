373b529ff01ce1c380ca4f56872e09b9de3d0705
Safia MSELLEK-ISMAILI <sms@cnam-lyon.peda>
2019-12-16 16:39:50 +0100

    displayed user's fake data in data base

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,53 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
+use DateTime;
+use Faker;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    private $encoder;
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+      $this->encoder = $encoder;
+    }
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
-        $manager->flush();
+      $faker = Faker\Factory::create('fr_FR');
+      $adminUser = new User();
+      $adminUser->setActive(true)
+        ->setRoles(['ROLE_ADMIN'])
+        ->setBirthDate(new DateTime('1990-02-16'))
+        ->setGender('Madame')
+        ->setLogin('Sophie')
+        ->setEmail('sophie@test.com')
+        ->setProfilPic($faker->imageUrl(150, 150))
+        ->setPassword(
+          $this->encoder->encodePassword(
+            $adminUser,
+            'Bobbybob57'
+          )
+        );
+      $manager->persist($adminUser);
+      for ($i = 0; $i < 50; $i++) {
+        $user = new User();
+        $login = $faker->userName;
+        $user->setActive($faker->boolean(75))
+          ->setBirthDate($faker->dateTimeBetween('-40 years', '-22 years'))
+          ->setEmail($faker->email)
+          ->setGender($faker->title())
+          ->setLogin($login)
+          ->setPassword($this->encoder->encodePassword(
+            $user,
+            'pass_' . $login
+          ))
+          ->setProfilPic($faker->imageUrl(150, 150));
+        $manager->persist($user);
+      }
+      $manager->flush();
     }
-}
+  }
